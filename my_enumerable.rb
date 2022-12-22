2c4467e8e3057534ba6125e5cc60861413b8c1ca
Shuhad786 <shuhad.loofer@gmail.com>
2022-12-22 12:52:06 +0200

    correct linter errors

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,7 +1,7 @@
 module MyEnumerable
   def all?
     output = true
-    each {|i| output = false unless yield i }
+    each { |i| output = false unless yield i }
     print 'all method: ', "\n", output, "\n"
   end
 
